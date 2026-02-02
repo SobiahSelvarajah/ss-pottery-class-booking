6f39857967e7e2aeb9b2038150566efa74b1ad4b
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-02-02 15:38:55 +0000

    Fixed Tailwind v4 config loading with @config, colors added to tailwind file

---
+++ tailwind.config.mjs
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+
+  safelist: [
+    "bg-clay-brown",
+    "bg-clay-dark",
+    "bg-clay-sand",
+    "bg-clay-light",
+    "text-neutral-white",
+    "text-neutral-dark",
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        clay: {
+          brown: "#A2672D",
+          dark: "#5B4D43",
+          sand: "#E0BE9A",
+          light: "#FFF8F1",
+        },
+        neutral: {
+          black: "#000000",
+          dark: "#404040",
+          mid: "#808080",
+          light: "#D2D2D2",
+          white: "#FFFFFF",
+        },
+      },
+    },
+  },
+};
