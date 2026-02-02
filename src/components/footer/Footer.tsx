5da8584c1404471ce318ccb6450e2471f344f961
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-02-02 23:09:45 +0000

    components: html added to footer section

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,34 @@
+
+
+export default function Footer() {
+    return(
+        <footer>
+            <div>
+                <h2>Connect with us for more information.</h2>
+                <nav aria-label="Footer navigation - Explore links">
+                    <h3>
+                        Explore
+                    </h3>
+                    <ul>
+                        <li><a href="#">Classes</a></li>
+                        <li><a href="#">Studios</a></li>
+                        <li><a href="#">Workshops</a></li>
+                    </ul>
+                </nav>
+                <nav aria-label="Footer navigation - Company links">
+                    <h3>About Kiln & Clay</h3>
+                    <ul>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">FAQs</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div>
+                <p>
+                    © 2026 Kiln & Clay. All rights reserved.
+                </p>
+            </div>
+        </footer>
+    )
+}
