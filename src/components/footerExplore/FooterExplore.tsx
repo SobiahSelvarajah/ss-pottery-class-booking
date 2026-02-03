a27970f4721c176f6483718e2ce224dd57268f0f
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-02-03 11:37:00 +0000

    components: footerExploreHeading created

--- src/components/footerExplore/FooterExplore.tsx
+++ src/components/footerExplore/FooterExplore.tsx
@@ -1,11 +1,10 @@
+import FooterExploreHeading from "../footerExploreHeading/FooterExploreHeading";
 
 
 export default function FooterExplore() {
     return(
         <nav aria-label="Footer navigation - Explore links">
-            <h3 className="font-heading text-lg mb-4 underline underline-offset-4">
-                Explore
-            </h3>
+            <FooterExploreHeading />
             <ul className="space-y-2 font-body text-neutral-light">
                 <li><a href="#" className="hover:text-neutral-white transition">Classes</a></li>
                 <li><a href="#" className="hover:text-neutral-white transition">Studios</a></li>
---
+++ src/components/footerExploreHeading/FooterExploreHeading.tsx
@@ -0,0 +1,9 @@
+
+
+export default function FooterExploreHeading() {
+    return(
+        <h3 className="font-heading text-lg mb-4 underline underline-offset-4">
+            Explore
+        </h3>
+    )
+}
