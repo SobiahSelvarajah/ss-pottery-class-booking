071a88ffa9d0999bcba262285bada14b1c0fe718
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-02-03 11:52:23 +0000

    components: footerCompanyLinks created

---
+++ src/components/footerCompanyLinks/FooterCompanyLinks.tsx
@@ -0,0 +1,23 @@
+
+
+export default function FooterCompanyLinks() {
+    return(
+        <ul className="space-y-2 font-body text-neutral-light">
+            <li>
+                <a href="#" className="hover:text-neutral-white transition">
+                    About
+                </a>
+            </li>
+            <li>
+                <a href="#" className="hover:text-neutral-white transition">
+                    Contact
+                </a>
+            </li>
+            <li>
+                <a href="#" className="hover:text-neutral-white transition">
+                    FAQs
+                </a>
+            </li>
+        </ul>
+    )
+}
