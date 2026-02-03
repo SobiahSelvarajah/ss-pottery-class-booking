4411d7e7e3d729d6055dead62500cbc8d623abce
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-02-03 23:51:31 +0000

    components: heroHeading created

---
+++ src/components/heroHeading/HeroHeading.tsx
@@ -0,0 +1,10 @@
+
+
+export default function HeroHeading() {
+    return(
+        <h1 className="font-heading text-4xl md:text-5xl 
+                       text-neutral-black max-w-md">
+            Discover Clay Art
+        </h1>
+    )
+}
--- src/components/heroIntro/HeroIntro.tsx
+++ src/components/heroIntro/HeroIntro.tsx
@@ -1,13 +1,11 @@
+import HeroHeading from "../heroHeading/HeroHeading";
 
 
 export default function HeroIntro() {
     return(
         <div className="flex flex-col gap-6 md:flex-row
                         md:items-center md:justify-between">
-            <h1 className="font-heading text-4xl md:text-5xl 
-                           text-neutral-black max-w-md">
-                Discover Clay Art
-            </h1>
+            <HeroHeading />
             <p className="font-body text-lg text-neutral-dark 
                           max-w-md md:text-right">
                 Explore local pottery classes and 
