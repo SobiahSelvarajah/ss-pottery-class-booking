781b9613a83b8467c0bbf227ccf06904fe062e41
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-02-03 10:20:58 +0000

    components: footerCopyright created

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,15 +1,12 @@
 import FooterContent from "../footerContent/FooterContent";
-
+import FooterCopyright from "../footerCopyright/FooterCopyright";
 
 export default function Footer() {
     return(
-        <footer className="bg-clay-dark text-neutral-white px-10 py-20 md:px-15 lg:px-25">
+        <footer className="bg-clay-dark text-neutral-white 
+                            px-10 py-20 md:px-15 lg:px-25">
             <FooterContent />
-            <div className="mt-16 border-t border-neutral-mid pt-6 text-left">
-                <p className="font-body text-sm text-neutral-light">
-                    © 2026 Kiln & Clay. All rights reserved.
-                </p>
-            </div>
+            <FooterCopyright />
         </footer>
     )
 }
---
+++ src/components/footerCopyright/FooterCopyright.tsx
@@ -0,0 +1,13 @@
+
+
+export default function FooterCopyright() {
+    return(
+        <div className="mt-16 border-t border-neutral-mid 
+                        pt-6 text-left">
+            <p className="font-body text-sm 
+                        text-neutral-light">
+                © 2026 Kiln & Clay. All rights reserved.
+            </p>
+        </div>
+    )
+}
