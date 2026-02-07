1b385ac9a815998f551afd5c63b03151d1564d24
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-02-07 23:28:15 +0000

    components: aboutOurStory created

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,6 @@
 import AboutIntro from "@/components/aboutIntro/AboutIntro";
+import AboutOurStory from "@/components/aboutOurStory/AboutOurStory";
+
 
 
 
@@ -15,18 +17,7 @@ export default function AboutPage() {
                         className="w-full h-96 object-cover rounded-lg"
                     />
                 </div>
-
-                <div>
-                    <h2 className="text-3xl font-medium mb-6">
-                        Our Story
-                    </h2>
-                    <p className="mb-4 text-neutral-mid leading-relaxed">
-                        Kiln & Clay was created to make pottery more accessible. We bring together independent studios, skilled makers, and curious beginners in one thoughtful space.
-                    </p>
-                    <p className="text-neutral-mid leading-relaxed">
-                        Whether you're touching clay for the first time or returning to a long-loved craft, we believe the process matters as much as the finished piece.
-                    </p>
-                </div>
+                <AboutOurStory />
             </section>
 
             <section className="max-w-6xl mx-auto px-10 py-20 grid md:grid-cols-2 gap-12 items-center">
