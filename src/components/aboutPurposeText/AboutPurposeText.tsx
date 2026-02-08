8e0ddabccc30e902008ddfc7c21eb83179e848cf
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-02-08 09:48:12 +0000

    components: aboutPurposeDesc created

--- src/components/aboutPurposeText/AboutPurposeText.tsx
+++ src/components/aboutPurposeText/AboutPurposeText.tsx
@@ -1,15 +1,12 @@
 import AboutPurposeHeading from "../aboutPurposeHeading/AboutPurposeHeading";
+import AboutPurposeDesc from "../aboutPurposeDesc/AboutPurposeDesc";
 
 
 export default function AboutPurposeText() {
     return(
         <div className="order-2 md:order-1 sm:text-left md:text-right">
             <AboutPurposeHeading />
-            <ul className="space-y-4 text-neutral-mid leading-relaxed">
-                <li>Curate high-quality pottery classes</li>
-                <li>Support independent studios and makers</li>
-                <li>Help people learn through hands-on experiences</li>
-            </ul>
+            <AboutPurposeDesc />
         </div>
     )
 }
