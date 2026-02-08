460b0d99dbacfbe146fdd0e0107a93bab6992bc2
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-02-08 01:15:19 +0000

    components: aboutStudioCard2 created

--- src/components/aboutStudioCards/AboutStudioCards.tsx
+++ src/components/aboutStudioCards/AboutStudioCards.tsx
@@ -1,19 +1,12 @@
 import AboutStudioCard1 from "../aboutStudioCard1/AboutStudioCard1";
+import AboutStudioCard2 from "../aboutStudioCard2/AboutStudioCard2";
 
 
 export default function AboutStudioCards() {
     return(
         <div className="grid md:grid-cols-3 gap-8">
             <AboutStudioCard1 />
-
-            <article className="bg-clay-light p-8 rounded-lg">
-                <h3 className="text-xl font-medium mb-3">
-                    Skilled Makers
-                </h3>
-                <p className="text-neutral-mid leading-relaxed">
-                    Classes are led by experienced ceramicists who love sharing their knowledge and process.
-                </p>
-            </article>
+            <AboutStudioCard2 />
 
             <article className="bg-clay-light p-8 rounded-lg">
                 <h3 className="text-xl font-medium mb-3">
