0746db7ffc2fb33531019574c5a297e12f51fe3f
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-02-08 08:26:10 +0000

    components: aboutStudioCard3 created

--- src/components/aboutStudioCards/AboutStudioCards.tsx
+++ src/components/aboutStudioCards/AboutStudioCards.tsx
@@ -1,5 +1,6 @@
 import AboutStudioCard1 from "../aboutStudioCard1/AboutStudioCard1";
 import AboutStudioCard2 from "../aboutStudioCard2/AboutStudioCard2";
+import AboutStudioCard3 from "../aboutStudioCard3/AboutStudioCard3";
 
 
 export default function AboutStudioCards() {
@@ -7,15 +8,7 @@ export default function AboutStudioCards() {
         <div className="grid md:grid-cols-3 gap-8">
             <AboutStudioCard1 />
             <AboutStudioCard2 />
-
-            <article className="bg-clay-light p-8 rounded-lg">
-                <h3 className="text-xl font-medium mb-3">
-                    Welcoming Spaces
-                </h3>
-                <p className="text-neutral-mid leading-relaxed">
-                    From beginners to experienced makers, our studios provide inclusive, supportive environments.
-                </p>
-            </article>
+            <AboutStudioCard3 />
         </div>         
     )
 }
