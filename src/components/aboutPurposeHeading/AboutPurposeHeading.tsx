9b39de98a5b9873a2fc06319bd8bcdf44308eca5
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-02-08 08:45:37 +0000

    components: aboutPurposeHeading created

---
+++ src/components/aboutPurposeHeading/AboutPurposeHeading.tsx
@@ -0,0 +1,9 @@
+
+
+export default function AboutPurposeHeading() {
+    return(
+        <h2 className="text-3xl font-medium mb-6">
+            Our Purpose
+        </h2>
+    )
+}
--- src/components/aboutPurposeText/AboutPurposeText.tsx
+++ src/components/aboutPurposeText/AboutPurposeText.tsx
@@ -1,11 +1,10 @@
+import AboutPurposeHeading from "../aboutPurposeHeading/AboutPurposeHeading";
 
 
 export default function AboutPurposeText() {
     return(
         <div className="order-2 md:order-1 sm:text-left md:text-right">
-            <h2 className="text-3xl font-medium mb-6">
-                Our Purpose
-            </h2>
+            <AboutPurposeHeading />
             <ul className="space-y-4 text-neutral-mid leading-relaxed">
                 <li>Curate high-quality pottery classes</li>
                 <li>Support independent studios and makers</li>
