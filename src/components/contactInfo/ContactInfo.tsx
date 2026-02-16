e92d795744dbc14fd398deea7c08214c67539ccc
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-02-16 23:19:22 +0000

    components: contactInfoContent created

--- src/components/contactInfo/ContactInfo.tsx
+++ src/components/contactInfo/ContactInfo.tsx
@@ -1,15 +1,11 @@
 import ContactInfoIntro from "../contactInfoIntro/ContactInfoIntro"
-
+import ContactInfoContent from "../contactInfoContent/ContactInfoContent"
 
 export default function ContactInfo() {
     return (
         <div className="space-y-8">
             <ContactInfoIntro />
-            <div className="space-y-4 text-sm">
-                <p>Email: hello@claystudios.com</p>
-                <p>Phone: +44 20 1234 5678</p>
-                <p>Opening Hours: Monday - Saturday, 9am - 7pm</p>
-            </div>
+            <ContactInfoContent />
         </div>
     )
 }
