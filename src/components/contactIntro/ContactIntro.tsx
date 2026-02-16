5ff619d3dc027f64221e0e1f9b82147b781e8e54
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-02-16 21:17:30 +0000

    components: contactIntroHeading created

--- src/components/contactIntro/ContactIntro.tsx
+++ src/components/contactIntro/ContactIntro.tsx
@@ -1,11 +1,10 @@
+import ContactIntroHeading from "../contactIntroHeading/ContactIntroHeading";
 
 
 export default function ContactIntro() {
     return (
         <section className="text-center mb-16">
-            <h1 className="text-3xl sm:text-4xl font-medium text-clay-dark">
-                Get in Touch
-            </h1>
+            <ContactIntroHeading />
             <p className="mt-4 max-w-2xl mx-auto text-neutral-mid">
                 Have a question about our studios, classes or bookings?
                 We'd love to hear from you!
---
+++ src/components/contactIntroHeading/ContactIntroHeading.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ContactIntroHeading() {
+    return (
+        <h1 className="text-3xl sm:text-4xl font-medium text-clay-dark">
+            Get in Touch
+        </h1>
+    )
+}
